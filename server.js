84f9525585b56bd8eb2b50fe530f70f5e4a992bb
alekseyes <good_4elovek@mail.ru>
2019-05-24 19:45:24 +0300

    2- коммит(Рыба =))

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const adminSeeder = require("./seed/adminSeeder");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yourappdb")
+  .then(() => {
+    console.log("connected to db");
+    adminSeeder();
+  })
+  .catch(() => console.log("Having problems connecting to the database"));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
