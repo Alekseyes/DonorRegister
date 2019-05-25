e53a7beef6271492fbcf6bb9bb4716fcaeea6b53
Rahman Abdurahmanov <raho0000@yandex.ru>
2019-05-25 15:38:17 +0300

    Form include.

--- client/src/components/form/userInfo.js
+++ client/src/components/form/userInfo.js
@@ -1,109 +1,64 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import classnames from "classnames";
-import { loginUser } from "../../actions/authActions";
+import axios from "axios";
 
-class userInfo extends Component {
-  constructor() {
+class userInfo extends Component
+{
+  constructor()
+  {
     super();
-    this.state = {
+
+    this.state =
+    {
       name: "",
       surname: "",
     };
   }
 
-  componentDidMount() {
-    const { auth, history } = this.props;
-    if (auth.isAuthenticated) {
-      history.push("/");
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/");
-    }
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  onChange = e =>
+  {
+    this.setState({[e.target.name]: e.target.value});
+    this.setState({[e.target.surname]: e.target.value});
   };
-  onSubmit = e => {
+
+  onSubmit = e =>
+  {
     e.preventDefault();
 
-    const userData = {
+    const userData =
+    {
       name: this.state.name,
-      password: this.state.password
+      surname: this.state.surname
     };
 
-    this.props.loginUser(userData);
+    console.log(this.state.name);
+    console.log(this.state.surname);
+
+    axios
+      .post("/api/testForm", {userData})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
   };
-  render() {
-    const { errors } = this.state;
-    return (
-      <div className="login">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your account</p>
-              <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className={classnames("form-control form-control-lg", {
-                      "is-invalid": errors.name
-                    })}
-                    placeholder="Name"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.onChange}
-                  />
-                  {errors.name && (
-                    <div className="invalid-feedback">{errors.name}</div>
-                  )}
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className={classnames("form-control form-control-lg", {
-                      "is-invalid": errors.password
-                    })}
-                    placeholder="Password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChange}
-                  />
-                  {errors.password && (
-                    <div className="invalid-feedback">{errors.password}</div>
-                  )}
-                </div>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
-            </div>
-          </div>
+
+  render()
+  {
+    return(
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Имя</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Ваше имя" name="name" value={this.state.name} onChange={this.onChange}/>
         </div>
-      </div>
+        
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Фамилия</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Ваша Фамилия" name="surname" value={this.state.surname} onChange={this.onChange}/>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Отправить</button>
+      </form>
     );
   }
 }
 
-userInfo.propTypes = {
-  loginUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
-
-export default connect(
-  mapStateToProps,
-  { loginUser }
-)(userInfo);
+export default userInfo;
