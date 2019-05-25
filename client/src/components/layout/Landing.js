22f1e882b339e6e54870125aee9d038618e07c72
Ilya <kompfloss@gmail.com>
2019-05-25 19:02:23 +0300

    Added check role

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -12,30 +12,28 @@ class Landing extends Component {
         <div className="dark-overlay landing-inner">
           <div className="container">
             <div className="row">
-              {!auth.isAuthenticated ? (
+              {(!auth.isAuthenticated) ? (
                 <div className="col-md-12 text-center">
                   <h1 className="display-3 mb-4">WebWeekend+</h1>
                   <p className="lead"> Create your own application</p>
                   <hr />
                   <Link to="/register" className="btn btn-lg btn-info mr-2">
-                    Sign Up5
+                    Sign Up
                   </Link>
                   <Link to="/login" className="btn btn-lg btn-light">
-                    Login6
-                  </Link>
-                  <Link to="/form" className="btn btn-lg btn-light">
-                    Form
+                    Login
                   </Link>
                 </div>
               ) : (
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">{`Привет ${
-                    auth.user.role + " " + auth.user.name
-                  } `}</h1>
-                  <h1 className="display-3 mb-4">{`по имени ${
-                    auth.user.name + "asd"
-                  } `}</h1>
+                (auth.user.role === "user") ? (
+                  <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">{`Привет ${auth.user.name} `}</h1>
+                  <Link to="/form" className="btn btn-lg btn-light">
+                    Заполнить анкету
+                  </Link>
                 </div>
+
+                ) : null // перенаправление
               )}
             </div>
           </div>
